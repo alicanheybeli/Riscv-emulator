e113e8cc9327d90c05319dce7d09e1a07d02bfc9
PhageSpace <43351812+PhageSpace@users.noreply.github.com>
2026-02-18 13:17:02 +0300

    full decoder

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -9,7 +9,7 @@ private:
     uint8_t rs1; //source register 1 15-19
     uint8_t rs2; //source register 2 20-24
     uint8_t funct7; // distinguishes R-type instructions 25-31
-    uint32_t imm; //immediate value
+    int32_t imm; //immediate value
 
 public:
     Instruction(uint32_t instruction);
@@ -21,19 +21,33 @@ Instruction::Instruction(uint32_t inst)
     opcode = inst & 0b1111111; //7 bits
     rd = (inst >> 7) & 0b11111; //5 bits
     funct3 = (inst >> 12) & 0b111; // 3 bits
-    rs1 = (inst >> 15) & 0b1111;
+    rs1 = (inst >> 15) & 0b11111;
     
     rs2 = 0;
-    funct3 = 0;
+    funct7 = 0;
     imm = 0;
 
-    switch ()
+
+    // we need to decode based on opcode because opcodes can change how an instruction is decoded. 
+    switch (opcode)
     {
-    case /* constant-expression */:
-        /* code */
+    case 0x33: // R types
+        rs2 = (inst >> 20) & 0x1F;
+        funct7 = (inst >> 25) & 0x7F;
         break;
-    
+    case 0x13: // I types
+    case 0x03: // Load instructions. we can get away with using the same decode here. the top 20 bits become the imm field.
+        imm = ((int32_t)inst >> 20); // we need to turn it into an int before shifting so that it does an arithmetic shift instead of a logical one. 
+        break;
+    case 0x23: //S-type
+        rs2 = (inst >> 20) & 0x1F;
+        int32_t top7 = ((int32_t)inst >> 25);
+        int32_t bottom5 = (inst >> 7) & 0x1F;
+        //reassemble the immideate
+        imm = (top7 << 5) | bottom5;
+        break;
     default:
+        
         break;
     }
 }
