fd84eda7d19bc08e3a5a0c5b5d04e3e5ba137512
PhageSpace <43351812+PhageSpace@users.noreply.github.com>
2026-02-27 22:04:22 +0300

    instruction printer

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,5 +1,57 @@
 #include <cstdint>
+#include <iostream>
 
+uint32_t test_program[] = {
+    0x00a00093, // ADDI x1, x0, 10
+    0x01400113, // ADDI x2, x0, 20
+    0x002081b3, // ADD  x3, x1, x2
+    0x00000073  // EBREAK
+};
+enum class Opcodes:uint8_t
+{
+    OP_REG = 0x33, //0110011 operations that take in 2 registers and output to a register
+    OP_IMM = 0x13, //0010011
+    ENV = 0x93, //1110011 enviroment
+};
+
+class Memory
+{
+private:
+    uint32_t ramsize = 1024*4;
+    uint8_t RAM[1024*4];
+    
+public:
+    uint32_t read32(uint32_t address);
+    uint8_t read8(uint32_t address);
+    void write32(uint32_t address,uint32_t value);
+    void read8(uint32_t address,uint8_t value);
+};
+
+uint32_t Memory::read32(uint32_t address){
+    if (address > ramsize)
+    {
+        std::cout << "attempted to reach to adress outside range:" << address;
+        return 0;
+    }
+    return static_cast<uint32_t>(read8(address)) | 
+    (static_cast<uint32_t>(read8(address))<< 8) |
+     (static_cast<uint32_t>( read8(address))<< 16) |
+     (static_cast<uint32_t>( read8(address))<< 24) |
+};
+uint8_t Memory::read8(uint32_t address)
+{
+    if (address > ramsize)
+    {
+        std::cout << "attempted to reach to adress outside range:" << address;
+        return 0;
+    }
+    
+    return RAM[address];
+};
+void Memory::write32(uint32_t address,uint32_t value)
+{
+
+};
 class Instruction
 {
 private:
@@ -14,6 +66,32 @@ private:
 public:
     Instruction(uint32_t instruction);
     ~Instruction();
+    void PrintInstruction(void){
+        std::cout << "opcode:" <<  opcode 
+        << " | rd: " << rd
+        << " | funct3: " << funct3
+        << " | rs1: " << rs1; 
+
+        
+
+    switch (opcode)
+    {
+    case 0x33: // R types
+        std::cout << " | rs2: "<< rs2 
+        << "funct7" << funct7;
+        break;
+    case 0x13: // I types
+    case 0x03: // Load instructions. we can get away with using the same decode here. the top 20 bits become the imm field.
+        std::cout << "| imm: "<< imm; // we need to turn it into an int before shifting so that it does an arithmetic shift instead of a logical one. 
+        break;
+    case 0x23: //S-type
+        std::cout << "| rs2: "<< rs2 << "| imm: "<< imm;
+        break;
+    default:
+        std::cout << "instruction printing is not implemented.";
+        break;
+    }
+    }
 };
 
 Instruction::Instruction(uint32_t inst)
@@ -41,13 +119,13 @@ Instruction::Instruction(uint32_t inst)
         break;
     case 0x23: //S-type
         rs2 = (inst >> 20) & 0x1F;
-        int32_t top7 = ((int32_t)inst >> 25);
-        int32_t bottom5 = (inst >> 7) & 0x1F;
+        int32_t top7 = ((int32_t)inst >> 25); //imm[11:5]
+        int32_t bottom5 = (inst >> 7) & 0x1F;//imm[4:0]
         //reassemble the immideate
         imm = (top7 << 5) | bottom5;
         break;
     default:
-        
+        std::cout << "instruction is not implemented.";
         break;
     }
 }
@@ -71,11 +149,11 @@ public:
     ~cpu();
     void step()
     {
-
+        
     }
     int fetch()
     {
-
+        
     }
     Instruction decode()
     {
